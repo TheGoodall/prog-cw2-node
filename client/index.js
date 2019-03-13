ebf45f4eac5f61165ad7692aaf64e61f4e1ea397
James Goodall <james.locked.goodall@gmail.com>
2019-03-13 22:45:52 +0000

    added work

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('click', async function(event){
     try{
-      let response = await fetch('http://127.0.0.1:809/list');
+      let response = await fetch('http://pc.d.frizlette.me:8090/home');
       let body = await response.text();
       document.getElementById('content').innerHTML=body;
     } catch(e) {
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+
+
 app.use(express.static('client'));
 
-let instruments = [ 'piano', 'concertina', 'double bass'];
 
-app.get('/list', function (req, resp){
+
+let instruments = [ 'Hello', 'testing', 'more testing'];
+
+
+
+app.get('/home', function (req, resp){
     resp.send(instruments);
 });
+
+
 
 app.listen(8090);
