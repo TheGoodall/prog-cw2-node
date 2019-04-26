304a1248708d5f50245e611a3eec2f8f61a8852d
James Goodall <james.locked.goodall@gmail.com>
2019-04-26 12:21:38 +0100

    started again with food

--- server.js
+++ server.js
@@ -7,26 +7,14 @@ app.use(express.static('client'));
 
 
 
-let posts = [ ['Hello','goodbye'], ['testing','stop testing'], ['more testing','less testing']];
 
 
 
-app.get('/posts', function (req, resp){
+
+app.get('/', function (req, resp){
     resp.send(posts);
 });
-app.get('/post', function(req, resp){
-    post_id = req.query.id
-    if (post_id != {}){
-        console.log(post_id);
-        console.log(typeof(post_id))
-        resp.send(posts)
 
-    } else {
-        resp.status(400)
-        resp.send({})
-    }
-    
-})
 
 
 
