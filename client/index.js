688a824dc50a297a08c218042476c68fd56c9ba1
James Goodall <james.locked.goodall@gmail.com>
2019-04-26 14:27:35 +0100

    copied client js from prog-potatoes

---
+++ client/index.js
@@ -0,0 +1,19 @@
+async function (event){
+  let response = await fetch('http://127.0.01:8090/list');
+  let body = await response.text();
+  document.getElementById('content').innerHTML = "<ul>";
+
+  let potatoes = JSON.parse(body);
+  for(let i = 0; i < potatoes.length; i++){
+    document.getElementById('content').innerHTML += "<li>" + potatoes[i] + "</li>";
+  }
+
+  document.getElementById('content').innerHTML += "</ul>";
+}
+
+
+document.getElementById('meal_butt').addEventListener('click', function(event){
+
+
+
+});
