4e781d14b066d42043200e3008a349bc210a63e2
James Goodall <james.locked.goodall@gmail.com>
2019-04-26 14:27:44 +0100

    started work on server

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const app = express();
 
 
 app.use(express.static('client'));
+app.use('/admin', express.static('admin'))
+
+let groups = [["Test Group", ]]
+
+let users = ["James"]
+
+let transactions = []
 
 
 
 
-
-
-
-app.get('/api/', function (req, resp){
-    resp.send(posts);
+app.get('/api/groups', function (req, resp){
+    resp.send(groups);
 });
-
+app.get('/api/transactions', function (req, resp){
+    resp.send(transactions)
+})
+app.get('/api/transactions/:groupid')
 
 
 
