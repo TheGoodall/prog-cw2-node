b9fec582d8f11197070d3c0804a78e1c6ef2c778
James Goodall <james.locked.goodall@gmail.com>
2019-04-27 16:39:52 +0100

    work

--- client/index.js
+++ client/index.js
@@ -1,45 +1,72 @@
 
 
+async function load_group(group){
+
+	
+	let response = await fetch("http://localhost:8090/api/transactions/"+group);
+	let body = await response.text();
+
+	let transactions = JSON.parse(body);
+	
+	document.getElementById("group").innerHTML = "";
+
+
+
+
+	for(let i = 0; i < transactions.length; i++){
+		document.getElementById("group").innerHTML += "<button type=\"button\" class=\"btn btn-secondary\">"+transactions[i]+"</button><br><br>";
+	}
+	$("#group").collapse("show");
+	$("#new").collapse("show");
+	
+	
+	
+}
 
 
 
 
 async function load_groups(){
 
-  let response = await fetch('http://localhost:8090/api/groups');
-  let body = await response.text();
+	let response = await fetch("http://localhost:8090/api/groups");
+	let body = await response.text();
 
-  let groups = JSON.parse(body);
+	let groups = JSON.parse(body);
 
-  for(let i = 0; i < groups.length; i++){
-    document.getElementById('groups').innerHTML += '<button type=button" class="btn btn-secondary">'+groups[i]+'</button><br><br>';
-  }
+
+	for(let i = 0; i < groups.length; i++){
+		document.getElementById("groups").innerHTML += "<button type=\"button\" id=\"group_butt_"+groups[i]+"\" class=\"btn btn-secondary\">"+groups[i]+"</button><br><br>";
+
+		document.getElementById("group_butt_"+groups[i]).addEventListener("click", function(){
+			load_group(groups[i]);
+		})
+	}
 }
 
 load_groups()
 
-
-document.getElementById('group').collapse
-document.getElementById('new').collapse
+$("#group").collapse("hide")
 
 
 
-document.getElementById('meal_butt').addEventListener('click', function(event){
-  document.getElementById("meal_butt").disabled = true;
-  document.getElementById("shop_butt").disabled = false;
 
-  document.getElementById("meal_form_div").style="display:block;";
-  document.getElementById("shop_form_div").style="display:none;";
+
+document.getElementById("meal_butt").addEventListener("click", function(event){
+	document.getElementById("meal_butt").disabled = true;
+	document.getElementById("shop_butt").disabled = false;
+
+	document.getElementById("meal_form_div").style="display:block;";
+	document.getElementById("shop_form_div").style="display:none;";
 
 
 });
 
-document.getElementById('shop_butt').addEventListener('click', function(event){
-  document.getElementById("shop_butt").disabled = true;
-  document.getElementById("meal_butt").disabled = false;
+document.getElementById("shop_butt").addEventListener("click", function(event){
+	document.getElementById("shop_butt").disabled = true;
+	document.getElementById("meal_butt").disabled = false;
 
-  document.getElementById("shop_form_div").style="display:block;";
-  document.getElementById("meal_form_div").style="display:none;";
+	document.getElementById("shop_form_div").style="display:block;";
+	document.getElementById("meal_form_div").style="display:none;";
 
 
 });
--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 
 
-app.use(express.static('client'));
-app.use('/admin', express.static('admin'))
+app.use(express.static("client"));
+app.use("/admin", express.static("admin"));
 
-let groups = [["Test Group", ]]
+let groups = ["Test Group", "Other Test Group"];
 
-let users = ["James"]
+let users = [["James", "Test user"]];
 
-let transactions = []
-
-
+let transactions = [[15, "James", "Test Group"], [25, "James", "Test Group"], [-17, "James", "Test Group"], [15, "James", "Other Test Group"], [15, "James", "Other Test Group"]];
 
 
-app.get('/api/groups', function (req, resp){
-    resp.send(groups);
+
+
+app.get("/api/groups", function (req, resp){
+	resp.send(groups);
 });
-app.get('/api/transactions', function (req, resp){
-    resp.send(transactions)
+app.get("/api/transactions", function (req, resp){
+	resp.send(transactions);
 })
-app.get('/api/transactions/:groupid')
+app.get("/api/transactions/:groupid", function (req, resp){
+	let group_transactions = [];
+	for(let i = 0; i < transactions.length; i++){
+		if (transactions[i][2] == req.params.groupid){
+			group_transactions.push(transactions[i]);
+		}
+	}
+	resp.send(group_transactions);
+	
+});
 
 
 
