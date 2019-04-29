3f13f4a9b5dab01225ddec0225fd540b143b3ea6
James Goodall <james.locked.goodall@gmail.com>
2019-04-29 21:46:05 +0100

    changes

--- server.js
+++ server.js
@@ -33,38 +33,34 @@ let groups = ["Test Group", "Other Test Group", "more test groups"];
 
 let transactions = [[15, "James", "Test Group"], [25, "James", "Test Group"], [-17, "James", "Test Group"], [15, "James", "Other Test Group"], [15, "James", "Other Test Group"]];
 
-async function getToken(){
-	
-	await fetch("https://frizlette.eu.auth0.com/oauth/token", {
-		method: "POST",
+
+
+function getToken(){
+
+	var request = require("request");
+
+	var options = { method: "POST",
+		url: "https://frizlette.eu.auth0.com/oauth/token",
 		headers: { "content-type": "application/x-www-form-urlencoded" },
-		form: { 
+		form: 
+		{
 			grant_type: "client_credentials",
 			client_id: "xGj40aZOgwwkdPCYzGsRqek3rnnr08lk",
 			client_secret: "3402emeVq4cRzRLiWzkt1Q2h2NmBN4uOjcCsZe4DCE48L3U_sSY_v_zPOZdeb2Fd",
 			audience: "https://frizlette.eu.auth0.com/api/v2/" 
 		}
-		
-	}).then(response => {
-		return response;
-	});
-
+	};
 	
-}
-
-async function getUserData(sub){
-	
-	await fetch("https://httpbin.org/post", {
-		headers: {"Authentication": "Bearer "+getToken()}
-	}).then(data => {
-		console.log(data)
-		return data;
+	request(options, function (error, response, body) {
+		if (error) throw new Error(error);
+		console.log(body);
 	});
+	
 }
 
 app.get("/api/groups", checkJwt, function (req, resp){
 	console.log(req.user.sub);
-	getUserData().then(data => {console.log(data)})
+	getToken()
 	
 	resp.send(groups);
 });
