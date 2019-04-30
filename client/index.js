5ff5306f87c24eb479b06d43195a4bcc2199375a
James Goodall <james.locked.goodall@gmail.com>
2019-04-30 10:49:48 +0100

    should fix the name on the clientside, cannot test until serverside complete: closes #10

--- client/index.js
+++ client/index.js
@@ -53,7 +53,11 @@ function load_group(group, accessToken){
 	callApi("/api/transactions/byGroup/"+group, accessToken).then(transactions => {
 		document.getElementById("transactions").innerHTML = "<tr><td>Amount</td><td>Name</td></tr>";
 		for(let i = 0; i < transactions.length; i++){
-			document.getElementById("transactions").innerHTML += "<tr><td>"+transactions[i][0]+"</td><td>"+transactions[i][1]+"</td></tr>";
+			document.getElementById("transactions").innerHTML += "<tr><td>"+transactions[i][0]+"</td><td id="+transactions[i][1]+"></td></tr>";
+			callApi("/api/users/byid/"+transactions[i][1], accessToken).then(data => {
+				document.getElementById(data.nickname)
+			})
+			
 		}
 		$("#transactions_collapse").collapse("show");
 		$("#group_collapse").collapse("show");
